e7430b6f2ee6cefdc6b6036e68532057cccba1e3
Shinichi <ukegawa0585@gmail.com>
2023-05-04 16:50:17 +0900

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('
 
 Route::get('/', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
